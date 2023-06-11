8c40f390a73c87a9703c92eb683c95fe22518f24
Fernando Carrascozzo <fernandocarrascozzo@gmail.com>
2023-06-11 01:26:19 -0300

    Esboço de tarefas

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,47 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: Scaffold(
-
-        ));
+      title: 'Lista de Tarefas',
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+      ),
+      home: Scaffold(
+          backgroundColor: Color(0xfff4f1e9),
+          appBar: AppBar(
+            backgroundColor: Color(0xff2b463c),
+            title: Text('Lista de Tarefas'),
+          ),
+          body: Container(
+            child: Stack(
+              children: [
+                Container(
+                  color: Color(0xff688f4e),
+                  height: 140,
+                ),
+                Container(
+                  height: 100,
+                  color: Color(0xfff4f1e9),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        color: Colors.black26,
+                        width: 72,
+                        height: 100,
+                      ),
+                      Text('Primeira Tarefa'),
+                      ElevatedButton(
+                          onPressed: (){},
+                          child: Icon(Icons.add)
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+              backgroundColor: Color(0xff2b463c), onPressed: () {})),
+    );
   }
 }
