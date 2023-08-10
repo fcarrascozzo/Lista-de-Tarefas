7e1857e19d057eb0bc2494726e005e29957d20d8
fcarrascozzo <fernandocarrascozzo@gmail.com>
2023-08-09 21:42:16 -0300

    Adicionando funções para procurar e obter tarefas

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -2,11 +2,9 @@ import 'package:flutter_lista_tarefas/data/tarefaDAO.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
-class DataBase {
-  static Future<Database> getDatabase() async {
+Future<Database> getDatabase() async {
     final String path = join(await getDatabasesPath(), 'tarefa.db');
     return openDatabase(path, onCreate: (db, version) {
       db.execute(TarefaDao.criarTabela);
     }, version: 1,);
   }
-}
--- lib/data/tarefaDAO.dart
+++ lib/data/tarefaDAO.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_lista_tarefas/components/tarefas.dart';
+import 'package:sqflite/sqflite.dart';
+
+import 'database.dart';
 
 class TarefaDao {
   static const String nomeTabela = 'Tarefas';
@@ -13,11 +17,36 @@ class TarefaDao {
 
   salvarTarefa(Tarefas tarefa) async {}
 
-  Future<List<Tarefas>> obterTarefasBanco() async {}
-
-  Future<List<Tarefas>> buscarTarefa(String nomeTarefa) async {}
-
-  deletarTarefa(String nomeTarefa) async {
-
+  Future<List<Tarefas>> obterTarefasBanco() async {
+    debugPrint('Acessando o obterTarefasBanco');
+    final Database bancoDeDados = await getDatabase();
+    final List<Map<String, dynamic>> retorno = await bancoDeDados.query(nomeTabela);
+    debugPrint('Procurando dados no banco de dados, encontrado: $retorno');
+    return toList(retorno);
   }
+
+  List<Tarefas> toList(List<Map<String, dynamic>> listaDeTarefas) {
+    debugPrint('Convertendo para Lista: ');
+    final List<Tarefas> tarefas = [];
+    for (Map<String, dynamic> cadaTarefa in listaDeTarefas) {
+      final Tarefas tarefa = Tarefas(cadaTarefa[tar_nome], cadaTarefa[tar_imagem], cadaTarefa[tar_dificuldade]);
+      tarefas.add(tarefa);
+    }
+    debugPrint('Lista de Tarefas populada $tarefas');
+    return tarefas;
+  }
+
+  Future<List<Tarefas>> buscarTarefa(String nomeTarefa) async {
+    debugPrint('acessando buscarTarefas');
+    final Database bancoDeDados = await getDatabase();
+    final List<Map<String, dynamic>> retorno = await bancoDeDados.query(
+      nomeTabela,
+      where: '$tar_nome = ?',
+      whereArgs: [nomeTarefa],
+    );
+    debugPrint('Tarefa encontrada: ${toList(retorno)}');
+    return toList(retorno);
+  }
+
+  deletarTarefa(String nomeTarefa) async {}
 }
